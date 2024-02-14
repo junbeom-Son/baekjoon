5e9dcae02f91a4eabe70600344ac03b9f3f6e5bd
junbeom-Son <89973303+junbeom-Son@users.noreply.github.com>
2024-02-14 10:29:40 +0900

    [Bronze V] Title: Hook, Time: 68 ms, Memory: 11196 KB -BaekjoonHub

---
+++ 백준/Bronze/10189. Hook/Hook.java
@@ -0,0 +1,10 @@
+public class Main {
+	public static void main(String[] args) {
+		StringBuilder answer = new StringBuilder();
+		answer.append("#  # #### #### #  #\n");
+		answer.append("#### #  # #  # # #\n");
+		answer.append("#### #  # #  # # #\n");
+		answer.append("#  # #### #### #  #\n");
+		System.out.print(answer);
+	}
+}
