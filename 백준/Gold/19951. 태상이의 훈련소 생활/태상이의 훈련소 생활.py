400738d8a9c571d0be17589b770327a725c8d959
junbeom-Son <89973303+junbeom-Son@users.noreply.github.com>
2024-06-29 21:39:22 +0900

    [Gold V] Title: 태상이의 훈련소 생활, Time: 276 ms, Memory: 67828 KB -BaekjoonHub

--- 백준/Gold/19951. 태상이의 훈련소 생활/태상이의 훈련소 생활.py
+++ 백준/Gold/19951. 태상이의 훈련소 생활/태상이의 훈련소 생활.py
@@ -1,34 +1,19 @@
 import sys
 input = sys.stdin.readline
 
-def getSize(N):
-    size = 2
-    while size < N:
-        size *= 2
-    return size
-
-def updateRange(index, left, right, a, b, k):
-    if left > b or right <= a: # 범위를 벗어난 경우 -> 아무 연산 x
-        return
-    if a <= left and right - 1 <= b: # 범위에 완전 포함된 경우 -> 해당 위치에 update check
-        tree[index] += k
-        return
-    updateRange(index * 2, left, (left + right) // 2, a, b, k)
-    updateRange(index * 2 + 1, (left + right) // 2, right, a, b, k)
-
 N, M = map(int, input().split())
-size = getSize(N)
-tree = [0] * (size * 2)
-heights = list(map(int, input().split()))
-for i in range(N):
-    tree[i+size] = heights[i]
+
+heights = [0] + list(map(int, input().split()))
 commands = list(list(map(int, input().split())) for _ in range(M))
-
+changes = [0] * (N + 2)
 for a, b, k in commands:
-    updateRange(1, 1, size + 1, a, b, k)
+    changes[a] += k
+    changes[b + 1] -= k
 
-for i in range(1, size):
-    tree[i * 2] += tree[i]
-    tree[i * 2 + 1] += tree[i]
+for i in range(1, N + 1):
+    changes[i] += changes[i - 1]
 
-print(' '.join(map(str, tree[size:size+N])))
+for i in range(1, N + 1):
+    heights[i] += changes[i]
+
+print(' '.join(map(str, heights[1:N + 1])))
