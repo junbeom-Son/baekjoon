0203e615c5f1af23c344693c8f20030f0de2e272
junbeom-Son <89973303+junbeom-Son@users.noreply.github.com>
2024-07-31 08:55:09 +0900

    [level 2] Title: 업그레이드 된 아이템 구하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/273711. 업그레이드 된 아이템 구하기/업그레이드 된 아이템 구하기.sql
@@ -0,0 +1,12 @@
+select item_id, item_name, rarity
+from item_info 
+where item_id in (
+    select item_id
+    from item_tree
+    where parent_item_id in (
+        select item_id
+        from item_info
+        where rarity = 'RARE'
+    )    
+)
+order by item_id desc;
