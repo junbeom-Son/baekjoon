195fa63a7ed3a9a4dd7a1023e7ad9b6b06296856
junbeom-Son <89973303+junbeom-Son@users.noreply.github.com>
2024-08-04 13:20:48 +0900

    [Gold I] Title: 구간 합 구하기, Time: 488 ms, Memory: 117036 KB -BaekjoonHub

--- 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.java
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.java
@@ -1,69 +1,78 @@
 import java.util.*;
 import java.io.*;
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		// n개의 숫자 입력
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		StringBuilder result = new StringBuilder();
-		int n = Integer.parseInt(st.nextToken());
-		int m = Integer.parseInt(st.nextToken());
-		int k = Integer.parseInt(st.nextToken());
-		int startIndex = getStartIndex(n);
-		
-		// 리프 노드 입력 및 세그먼트 트리 초기화
-		long[] numbers = new long[startIndex * 2];
-		for (int i = 0; i < n; ++i) {
-		    numbers[i + startIndex] = Long.parseLong(br.readLine());
-		}
-		for (int i = startIndex - 1; i > 0; --i) {
-		    numbers[i] = numbers[i * 2] + numbers[i * 2 + 1];
-		}
-		
-		// 숫자를 변경하거나, 구간 합을 구하기
-		for (int i = 0; i < m + k; ++i) {
-		    st = new StringTokenizer(br.readLine());
-		    int a = Integer.parseInt(st.nextToken());
-		    int b = Integer.parseInt(st.nextToken());
-		    long c = Long.parseLong(st.nextToken());
-		    b = convertToStartIndex(startIndex, b);
-		    if (a == 1) { // b 번째 숫자를 c로 변경하기
-		        numbers[b] = c;
-		        b /= 2;
-		        while (b > 0) {
-		            numbers[b] = numbers[b * 2] + numbers[b * 2 + 1];
-		            b /= 2;
-		        }
-		    } else { // b 번째 부터 c 번째 숫자까지 구간 합 구하기
-		        int start = b;
-		        int end = convertToStartIndex(startIndex, (int) c);
-		        long answer = 0L;
-		        while (start <= end) {
-		            if (start % 2 == 1) {
-		                answer += numbers[start];
-		            }
-		            if (end % 2 == 0) {
-		                answer += numbers[end];
-		            }
-		            start = (start + 1) / 2;
-		            end = (end - 1) / 2;
-		        }
-		        result.append(answer).append('\n');
-		    }
-		}
-		System.out.println(result);
-		br.close();
-	}
-	
-	private static int getStartIndex(int n) {
-	    int startIndex = 2;
-	    while (startIndex < n) {
-	        startIndex *= 2;
-	    }
-	    return startIndex;
-	}
-	
-	private static int convertToStartIndex(int startIndex, int index) {
-	    return startIndex + index - 1;
-	}
+
+    static void init(long[] a, long[] tree, int node, int start, int end) {
+        if (start == end) {
+            tree[node] = a[start];
+        } else {
+            init(a, tree, node * 2, start, (start + end) / 2);
+            init(a, tree, node * 2 + 1, (start + end) / 2 + 1, end);
+            tree[node] = tree[node * 2] + tree[node * 2 + 1];
+        }
+    }
+
+    // start, end : node의 커버 범위
+    // left, right: 탐색을 해야 하는 범위
+    static long query(long[] tree, int node, int start, int end, int left, int right) {
+        if (end < left || right < start) { // 전혀 겹치지 않는 경우
+            return 0;
+        }
+        if (left <= start && end <= right) { // left와 right가 start, end를 모두 감싸는 경우
+            return tree[node];
+        }
+        long leftSum = query(tree, node * 2, start, (start + end) / 2, left, right);
+        long rightSum = query(tree, node * 2 + 1, (start + end) / 2 + 1, end, left, right);
+        return leftSum + rightSum;
+    }
+
+    static void updateTree(long[] tree, int node, int start, int end, int index, long diff) {
+        if (index < start || end < index) {
+            return;
+        }
+        tree[node] += diff;
+        if (start != end) { // 하나의 index만 update하기 때문에, 리프노드가 아니면 리프노드까지 업데이트 진행
+            updateTree(tree, node * 2, start, (start + end) / 2, index, diff);
+            updateTree(tree, node * 2 + 1, (start + end) / 2 + 1, end, index, diff);
+        }
+    }
+
+    static void update(long[] a, long[] tree, int N, int index, long value) {
+        long diff = value - a[index];
+        a[index] = value;
+        updateTree(tree, 1, 0, N - 1, index, diff);
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        long[] a = new long[N];
+        for (int i = 0; i < N; i++) {
+            a[i] = Long.parseLong(br.readLine());
+        }
+        int height = (int) Math.ceil(Math.log(N) / Math.log(2));
+        int treeSize = (1 << height + 1);
+        long[] tree = new long[treeSize];
+        init(a, tree, 1, 0, N - 1);
+        StringBuilder answer = new StringBuilder();
+        for (int i = 0; i < M + K; ++i) {
+            st = new StringTokenizer(br.readLine(), " ");
+            int command = Integer.parseInt(st.nextToken());
+            if (command == 1) {
+                int number1 = Integer.parseInt(st.nextToken());
+                long number2 = Long.parseLong(st.nextToken());
+                update(a, tree, N, number1 - 1, number2);
+            } else {
+                int number1 = Integer.parseInt(st.nextToken());
+                int number2 = Integer.parseInt(st.nextToken());
+                answer.append(query(tree, 1, 0, N - 1, number1 - 1, number2 - 1)).append("\n");
+            }
+        }
+        br.close();
+        System.out.print(answer);
+    }
 }
