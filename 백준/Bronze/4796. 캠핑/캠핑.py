a30bc65e7915fc5186757526ebc0076399e5ed36
junbeom-Son <89973303+junbeom-Son@users.noreply.github.com>
2024-08-19 11:17:31 +0900

    [Bronze I] Title: 캠핑, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/4796. 캠핑/캠핑.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+caseNumber = 1
+answers = []
+while True:
+    L, P, V = map(int, input().split())
+    if L == 0 and P == 0 and V == 0:
+        break
+    count = V // P * L
+    V %= P
+    count += min(V, L)
+    answers.append(f'Case {caseNumber}: {count}')
+    caseNumber += 1
+
+print('\n'.join(answers))
