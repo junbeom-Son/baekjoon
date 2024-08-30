bf7d767284c9157b2da6e48f3631e28d3cd63bf3
junbeom-Son <89973303+junbeom-Son@users.noreply.github.com>
2024-08-30 09:19:08 +0900

    [Gold V] Title: 물병, Time: 2448 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/1052. 물병/물병.py
+++ 백준/Gold/1052. 물병/물병.py
@@ -1,32 +1,15 @@
-def initialize(N):
-    counts = set()
-    num = 1
-    while num <= N:
-        if num & N > 0:
-            counts.add(num)
-        num <<= 1
-
-    return counts
-
-def addBottle(bottles):
-    bottle = 1
-    while True:
-        if bottle not in bottles:
-            break
-        bottles.remove(bottle)
-        bottle <<= 1
-
-    bottles.add(bottle)
+def isPossible(N, K):
+    return bin(N).count('1') <= K
 
 def solution(N, K):
-    bottles = initialize(N)
     count = 0
-    while len(bottles) > K:
-        addBottle(bottles)
+    while True:
+        if isPossible(N, K):
+            break
         count += 1
+        N += 1
 
     return count
-
 
 N, K = map(int, input().split())
 print(solution(N, K))
