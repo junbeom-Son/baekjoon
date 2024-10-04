ce08c8db27768eeb3020b1f786a5c25187229d82
junbeom-Son <89973303+junbeom-Son@users.noreply.github.com>
2024-10-04 22:03:40 +0900

    [Gold V] Title: 개똥벌레, Time: 456 ms, Memory: 79976 KB -BaekjoonHub

--- 백준/Gold/3020. 개똥벌레/개똥벌레.py
+++ 백준/Gold/3020. 개똥벌레/개똥벌레.py
@@ -1,34 +1,29 @@
 import sys
 input = sys.stdin.readline
 
-def get_count(numbers, height):
-    left, right = 0, len(numbers) - 1
-    index = -1
-    while left <= right:
-        mid = (left + right) // 2
-        if numbers[mid] >= height:
-            index = mid
-            left = mid + 1
-        else:
-            right = mid - 1
-    return index + 1
-
 N, H = map(int, input().split())
 heights = list(int(input()) for _ in range(N))
 from_bottom = heights[::2]
 from_top = heights[1::2]
-from_bottom.sort()
-from_bottom.reverse()
-from_top.sort()
-from_top.reverse()
-min_count, count = 10000000, 0
-for i in range(1, H + 1):
-    current_count = get_count(from_bottom, i)
-    current_count += get_count(from_top, (H - i) + 1)
-    if current_count < min_count:
-        min_count = current_count
-        count = 1
-    elif current_count == min_count:
-        count += 1
+from_bottom_counts = [0] * (H + 1)
+from_top_counts = [0] * (H + 1)
+for number in from_bottom:
+    from_bottom_counts[number] += 1
+for number in from_top:
+    from_top_counts[number] += 1
 
+for i in range(1, H + 1):
+    from_bottom_counts[i] += from_bottom_counts[i - 1]
+    from_top_counts[i] += from_top_counts[i - 1]
+
+min_count, count = 100000000, 0
+for i in range(1, H + 1):
+    bottom_count = from_bottom_counts[H] - from_bottom_counts[i - 1]
+    top_count = from_top_counts[H] - from_top_counts[H - i]
+    total_count = bottom_count + top_count
+    if total_count < min_count:
+        min_count = total_count
+        count = 1
+    elif total_count == min_count:
+        count += 1
 print(min_count, count)
